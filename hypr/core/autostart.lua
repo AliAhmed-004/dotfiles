f312c1e0e431b8ad538396af8ee816d9d1b9ec55
AliAhmed-004 <eliehmed908@gmail.com>
2026-07-14 13:08:40 +0500

    added wallpaper config

--- hypr/core/autostart.lua
+++ hypr/core/autostart.lua
@@ -3,13 +3,12 @@
 -- =============================
 
 hl.on("hyprland.start", function()
+	hl.exec_cmd("waybar")
+	hl.exec_cmd("hyprpaper")
 
-    hl.exec_cmd("waybar")
-
-    -- Future additions:
-    -- hl.exec_cmd("hyprpaper")
-    -- hl.exec_cmd("swaync")
-    -- hl.exec_cmd("hypridle")
-    -- hl.exec_cmd("nm-applet")
-
+	-- Future additions:
+	-- hl.exec_cmd("hyprpaper")
+	-- hl.exec_cmd("swaync")
+	-- hl.exec_cmd("hypridle")
+	-- hl.exec_cmd("nm-applet")
 end)
